d165dc966d48ae21535d12b5b2999052b44b2f9c
Owen <ow.codina@duocuc.cl>
2026-05-25 19:29:06 -0400

    se agrega a la carpeta Ranking coon los models y DTO´S

---
+++ msvc-ranking/src/main/java/com/esports/msvc_ranking/models/dtos/ActualizarRankingRequestDTO.java
@@ -0,0 +1,25 @@
+package com.esports.msvc_ranking.models.dtos;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ActualizarRankingRequestDTO {
+
+    @NotNull(message = "El ganadorId no puede ser nulo")
+    private Long ganadorId;
+
+    @NotNull(message = "El perdedorId no puede ser nulo")
+    private Long perdedorId;
+
+    @NotNull(message = "El puntajeGanador no puede ser nulo")
+    private Integer puntajeGanador;
+
+    @NotNull(message = "El puntajePerdedor no puede ser nulo")
+    private Integer puntajePerdedor;
+}
---
+++ msvc-ranking/src/main/java/com/esports/msvc_ranking/models/dtos/RankingResponseDTO.java
@@ -0,0 +1,19 @@
+package com.esports.msvc_ranking.models.dtos;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class RankingResponseDTO {
+
+    private Long rankingId;
+    private Long torneoId;
+    private Long participanteId;
+    private Integer puntos;
+    private Integer victorias;
+    private Integer derrotas;
+    private Integer diferencia;
+    private Integer posicion;
+    private LocalDateTime updatedAt;
+}
