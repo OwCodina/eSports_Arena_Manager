6cc3552441203896b16096e9950720a7d1ecc2ea
seba9 <se.cabreral@duocuc.cl>
2026-05-25 20:54:08 -0400

    se agregra ResultadoDTO

---
+++ msvc-ranking/src/main/java/com/esports/msvc_ranking/models/dtos/ResultadoDTO.java
@@ -0,0 +1,19 @@
+package com.esports.msvc_ranking.models.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter @Setter @NoArgsConstructor
+public class ResultadoDTO {
+    private Long resultadoId;
+    private Long partidaId;
+    private Long ganadorId;
+    private Integer puntajeA;
+    private Integer puntajeB;
+    private String estadoValidacion;
+    private LocalDateTime fechaRegistro;
+}
