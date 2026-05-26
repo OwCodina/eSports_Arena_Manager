6c70b472882e542a653c90db42057d7228037115
seba9 <se.cabreral@duocuc.cl>
2026-05-25 21:21:11 -0400

    se ponen de manera explicita las url de varios client, game, match y user

--- msvc-result/src/main/java/com/esports/msvc_result/clients/MatchClient.java
+++ msvc-result/src/main/java/com/esports/msvc_result/clients/MatchClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-@FeignClient(name = "msvc-match", url = "${msvc.match.url}")
+@FeignClient(name = "msvc-match", url = "http://localhost:8016")
 public interface MatchClient {
 
     @GetMapping("/api/v1/partidas/{id}")
--- msvc-team/src/main/java/com/esports/msvc_team/clients/GameClient.java
+++ msvc-team/src/main/java/com/esports/msvc_team/clients/GameClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "msvc-game", url = "${msvc.game.url}")
+@FeignClient(name = "msvc-game", url = "http://localhost:8010")
 public interface GameClient {
 
     @GetMapping("/api/v1/juegos/{id}")
